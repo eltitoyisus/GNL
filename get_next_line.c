724f103a7409d0a604b349f25c35801ac91596f6
eltitoyisus <minecraftgamingra@gmail.com>
2024-11-04 16:51:15 +0100

    added

--- get_next_line.c
+++ get_next_line.c
@@ -6,61 +6,35 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 14:12:13 by marvin            #+#    #+#             */
-/*   Updated: 2024/11/04 16:38:34 by marvin           ###   ########.fr       */
+/*   Updated: 2024/11/04 16:51:12 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-/*
-int	ft_line_len(int fd)
-{
-	int		len;
-	char	buffer;
-	int		bytes_read;
-
-	len = 0;
-	bytes_read = 1;
-	while (bytes_read > 0)
-	{
-		bytes_read = read(fd, &buffer, 1);
-		if (bytes_read > 0)
-		{
-			len++;
-			if (buffer == '\n')
-				bytes_read = 0;
-		}
-	}
-	return (len);
-}
-*/
 
 char	*ft_read(int fd, char *str)
 {
-    char	*buffer;
-    int		total_read;
+	char	*bff;
+	int		bytes;
 
-    str = ft_strjoin(str, buffer);
-    buffer = malloc(sizeof(char *) * (BUFFER_SIZE + 1));
-    if (!buffer)
-        return (NULL);
-    total_read = 1;
-    while (!ft_strchr(str, '\n') && total_read != 0)
-    {
-        total_read = read(fd, buffer, BUFFER_SIZE);
-        if (total_read == -1)
-        {
-            free(buffer);
-            return (NULL);
-        }
-        buffer[total_read] = '\0';
-        if (!str)
-        {
-            free(buffer);
-            return (NULL);
-        }
-    }
-    free(buffer);
-    return (NULL);
+	bff = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!bff)
+		return (NULL);
+	bytes = 1;
+	while (!ft_strchr(str, '\n') && bytes != 0)
+	{
+		bytes = read(fd, bff, BUFFER_SIZE);
+		if (bytes == -1)
+		{
+			free(bff);
+			free(str);
+			return (NULL);
+		}
+		bff[bytes] = '\0';
+		str = ft_strjoin(str, bff);
+	}
+	free(bff);
+	return (str);
 }
 char	*ft_line(char *str)
 {
