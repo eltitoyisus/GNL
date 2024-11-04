1cf850729abd875d73bf5c48d6937d30478dfa32
eltitoyisus <minecraftgamingra@gmail.com>
2024-11-04 18:36:00 +0100

    added

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 14:12:13 by marvin            #+#    #+#             */
-/*   Updated: 2024/11/04 18:19:30 by marvin           ###   ########.fr       */
+/*   Updated: 2024/11/04 18:35:57 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,67 +39,70 @@ char	*ft_read(int fd, char *str)
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	size_t	len1;
-	size_t	len2;
-	char	*new_str;
+    size_t	len1;
+    size_t	len2;
+    char	*new_str;
 
-	if (!s1 || !s2)
-		return (NULL);
-	len1 = ft_strlen(s1);
-	len2 = ft_strlen(s2);
-	new_str = (char *)malloc((len1 + len2 + 1) * sizeof(char));
-	if (!new_str)
-		return (NULL);
-	ft_memcpy(new_str, s1, len1);
-	ft_memcpy(new_str + len1, s2, len2 + 1);
-	return (new_str);
+    if (!s1 || !s2)
+        return (NULL);
+    len1 = ft_strlen(s1);
+    len2 = ft_strlen(s2);
+    new_str = (char *)malloc((len1 + len2 + 1) * sizeof(char));
+    if (!new_str)
+        return (NULL);
+    ft_memcpy(new_str, s1, len1);
+    ft_memcpy(new_str + len1, s2, len2 + 1);
+    return (new_str);
 }
 
 static char	*ft_line(char *str)
 {
-	int		i;
-	char	*line;
+    int		i;
+    char	*line;
 
-	i = 0;
-	if (!str[i])
-		return (NULL);
-	while (str[i] && str[i] != '\n')
-		i++;
-	line = ft_substr(str, 0, i + 1);
-	return (line);
+    if (!str)
+        return (NULL);
+    i = 0;
+    while (str[i] && str[i] != '\n')
+        i++;
+    line = ft_substr(str, 0, i + 1);
+    return (line);
 }
 
-static char	*ft_nextstr(char *str)
+char	*ft_nextstr(char *str)
 {
-	int		i;
-	char	*nextstr;
+    int		i;
+    char	*nextstr;
 
-	i = 0;
-	while (str[i] && str[i] != '\n')
-		i++;
-	if (!str[i])
-	{
-		free(str);
-		return (NULL);
-	}
-	nextstr = ft_substr(str, i + 1, ft_strlen(str) - i);
-	free(str);
-	return (nextstr);
+    if (!str)
+        return (NULL);
+    i = 0;
+    while (str[i] && str[i] != '\n')
+        i++;
+    if (!str[i])
+    {
+        free(str);
+        return (NULL);
+    }
+    nextstr = ft_substr(str, i + 1, ft_strlen(str) - i);
+    free(str);
+    return (nextstr);
 }
 
 char	*get_next_line(int fd)
 {
-	static char	*str;
-	char		*line;
+    static char	*str;
+    char		*line;
 
-	if (fd < 0 || BUFFER_SIZE <= 0)
-		return (NULL);
+	str = NULL;
+    if (fd < 0 || BUFFER_SIZE <= 0)
+        return (NULL);
 
-	str = ft_read(fd, str);
-	if (!str)
-		return (NULL);
+    str = ft_read(fd, str);
+    if (!str)
+        return (NULL);
 
-	line = ft_line(str);
-	str = ft_nextstr(str);
-	return (line);
+    line = ft_line(str);
+    str = ft_nextstr(str);
+    return (line);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -28,6 +28,7 @@ char	*ft_strchr(const char *s, int c);
 size_t	ft_strlen(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strdup(const char *s);
+char	*ft_nextstr(char *str);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 char	*ft_strjoin(char const *s1, char const *s2);
 
