f8f36fd573fb61001eb74b05a3aef49f378ab037
eltitoyisus <minecraftgamingra@gmail.com>
2024-11-13 13:59:25 +0100

    added

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,35 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 14:12:13 by marvin            #+#    #+#             */
-/*   Updated: 2024/11/12 21:22:17 by marvin           ###   ########.fr       */
+/*   Updated: 2024/11/13 13:57:47 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+char	*ft_readndfree(int fd, char *buffer)
+{
+	char	*s;
+	int		bytes;
+
+	s = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!s)
+		return (NULL);
+	bytes = 1;
+	while (!ft_strchr(buffer, '\n') && bytes != 0)
+	{
+		bytes = read(fd, s, BUFFER_SIZE);
+		if (bytes < 0)
+		{
+			free(s);
+			return (NULL);
+		}
+		s[bytes] = '\0';
+		buffer = ft_strjoin(buffer, s);
+	}
+	free(s);
+	return (buffer);
+}
 
 char	*ft_getline(char *buffer)
 {
@@ -57,58 +81,6 @@ char	*ft_freebuff(char *buffer)
 		str[j++] = buffer[i++];
 	str[j] = '\0';
 	free(buffer);
-	return (str);
-}
-
-char	*ft_readndfree(int fd, char *buffer)
-{
-	char	*s;
-	int		bytes;
-
-	s = malloc((BUFFER_SIZE + 1) * sizeof(char));
-	if (!s)
-		return (NULL);
-	bytes = 1;
-	while (!ft_strchr(buffer, '\n') && bytes != 0)
-	{
-		bytes = read(fd, s, BUFFER_SIZE);
-		if (bytes < 0)
-		{
-			free(s);
-			return (NULL);
-		}
-		s[bytes] = '\0';
-		buffer = ft_strjoin(buffer, s);
-	}
-	free(s);
-	return (buffer);
-}
-
-char	*ft_strjoin(char *s1, char *s2)
-{
-	size_t	i;
-	size_t	j;
-	char	*str;
-
-	if (!s1)
-	{
-		s1 = malloc(sizeof(char));
-		s1[0] = '\0';
-	}
-	str = malloc(sizeof(char) * ((ft_strlen(s1) + ft_strlen(s2)) + 1));
-	if (!str)
-		return (NULL);
-	i = 0;
-	j = 0;
-	while (s1[i])
-	{
-		str[i] = s1[i];
-		i++;
-	}
-	while (s2[j])
-		str[i++] = s2[j++];
-	str[i] = '\0';
-	free(s1);
 	return (str);
 }
 
